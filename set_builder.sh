01422d847135759b654b40cd592f34821702542e
levirre <krischeval@outlook.com>
2021-04-12 16:01:38 -0700

    fix pattern matching

--- set_builder.sh
+++ set_builder.sh
@@ -4,7 +4,14 @@ read -p 'Paste Set URL Source: '  URL
 echo Entered: 
 echo $URL
 ##
-LIST=$(curl $URL | grep '{{CardList/TD*' | sed 's/{{CardList*TD|//;s/..$//;s/ /_/g'  | tr '/' '-' | cut -d "|" -f 2- )
-SET=$(echo "$LIST" |  head -n 1 | grep -o 'D-....' )
+LIST=$(curl $URL | grep '{{CardList*' | grep -v 'header\|footer'| sed 's/..$//;s/ /_/g' | cut -d "|" -f 2- | tr '/' '-'  )
+SET=$(echo "$LIST" |  head -n 1 | grep -o '.-....' )
 
-echo "$LIST" > $SET.txt
+printf "$LIST" 
+
+##{{CardList/TD
+##{{CardList/header/TD}}
+##{{CardListV|
+##{{CardList/header/V}}
+
+
