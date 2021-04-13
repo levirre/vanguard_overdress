dec610ece755db58569f185919d3cbe0747de3a5
levirre <krischeval@outlook.com>
2021-04-13 00:09:50 -0700

    Create initial electron window

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 //main.js
-//const electron = require('electron');
+const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
 
+// This is like specific importing functions from a module in Python
+// from Cirle.py import GetArea
 
-//const {app, BrowserWindow } = electron;
+//import app and BrowserWindow modules from Electron
+const {app, BrowserWindow, Menu } = electron;
 
-console.log("hello");
+let mainWindow;
+
+// Listen for the app to be ready
+app.on('ready',function(){
+    //create new window
+    mainWindow = new BrowserWindow({});
+    //load html file into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'mainWindow.html'),
+        protocol:'file',
+        slashes: true
+        // loads ./mainWindow.html
+    }));
+    //This create the top bar header --File Edit Selection... etc
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed',function(){app.quit});
+
+});
+
+
+const mainMenuTemplate = [{
+    label:'File',
+    submenu:[{label: 'Start New Game'},{label: 'Connect'},{label: 'Reconnect'},{label: 'Quit',click(){app.quit()}  }]
+}]
+
 
