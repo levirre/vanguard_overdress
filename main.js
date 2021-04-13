c242406a12c9c2dcf34c997338796c82f31d2af3
levirre <krischeval@outlook.com>
2021-04-12 23:40:31 -0700

    Start electron implementation

---
+++ main.js
@@ -0,0 +1,11 @@
+//main.js
+//const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+
+
+//const {app, BrowserWindow } = electron;
+
+console.log("hello");
+
